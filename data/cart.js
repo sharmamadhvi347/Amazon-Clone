ef8dd6b2a0471da8511b715d938e003f91e73da1
Madhvi Sharma <sharmamadhvi347@gmail.com>
2025-07-11 18:50:59 +0530

    delete button interactive

--- data/cart.js
+++ data/cart.js
@@ -27,3 +27,15 @@ export function addToCart(productId) {
     }
 
 }
+
+export function removeFromCart(){
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+}
