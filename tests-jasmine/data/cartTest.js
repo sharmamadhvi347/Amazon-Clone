d0c25aa4c88b3e40bc298881f856c7edb44f07fd
Madhvi Sharma <sharmamadhvi347@gmail.com>
2025-07-26 07:37:50 +0530

    Create automated tests for addToCart

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,30 @@
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+
+describe ('test suite: addToCart', () => {
+    it('adds an existing product to the cart', () => {
+        spyOn(localStorage,'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity :1,
+                deliveryOption : '1'
+            }]);
+        });
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+    });
+
+    it('adds a new product to the cart', () => {
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage,'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        });
+        loadFromStorage();
+
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+    });
+});
